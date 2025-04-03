a3d02997acbb80719fc2e9035e020d27d4b33072
komaljhaaaa <kjha1@terpmail.umd.edu>
2025-04-02 22:45:26 -0400

    goodbye function

--- test code.py
+++ test code.py
@@ -1,9 +1,13 @@
 def greet_user(name):
     print(f"Hello, {name}! Welcome to Exercise 6.")
 
+def say_goodbye(name):
+    print(f"Goodbye, {name}! Thanks for running the script.")
+
 def main():
     user_name = input("Enter your name: ")
     greet_user(user_name)
+    say_goodbye(user_name)
 
 if __name__ == "__main__":
     main()
