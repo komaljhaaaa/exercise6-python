4d32c15e7cb8b94ba15d14b2ce36c4586cd23c5f
komaljhaaaa <kjha1@terpmail.umd.edu>
2025-04-02 22:32:44 -0400

    test

---
+++ test code.py
@@ -0,0 +1,9 @@
+def greet_user(name):
+    print(f"Hello, {name}! Welcome to Exercise 6.")
+
+def main():
+    user_name = input("Enter your name: ")
+    greet_user(user_name)
+
+if __name__ == "__main__":
+    main()
